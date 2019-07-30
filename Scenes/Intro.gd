22efad8cc02f54a7deed4c5c7ce97a1b6e5b9308
Davide Cristini <davcri91@gmail.com>
2019-07-30 22:47:16 +0100

    Fix positioning on resize/multiple resolutions and add movement tween

--- Scenes/Intro.gd
+++ Scenes/Intro.gd
@@ -4,9 +4,30 @@ tool
 export(bool) var updateScreen = false
 export(bool) var reset = false
 
+export(int) var distance = 750
+export(int) var duration = 5
+
 func _ready():
+	resizeEverything()
+	fillHorizontalSpace()
+	# on resize, update the background
+	get_viewport().connect("size_changed", self, "resizeEverything")
+	
+	for c in get_children():
+		if c.name != "Tween":
+			print(typeof(c))
+			c.get_node("Tween").interpolate_property(c, "position", c.position, c.position + Vector2(-distance, 0), duration, Tween.TRANS_CUBIC,Tween.EASE_IN_OUT)
+			c.get_node("Tween").start()
+
+func resizeEverything():
+	removeDuplicateSprites()
 	updateHeight()
 	fillHorizontalSpace()
+
+func removeDuplicateSprites():
+	for c in get_children():
+		if c != $SkylineA:
+			c.queue_free()
 
 func _process(delta):
 	if updateScreen:
@@ -30,5 +51,6 @@ func fillHorizontalSpace():
 func updateHeight():
 	var missing_height = get_viewport_rect().size.y - $SkylineA.get_rect().size.y
 	if missing_height > 0 :
+		# scale sprite
 		var scale_factor = ($SkylineA.get_rect().size.y + missing_height) / $SkylineA.get_rect().size.y
 		$SkylineA.scale = Vector2(scale_factor, scale_factor)
