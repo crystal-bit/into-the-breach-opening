bf4577430ca3d3d4173a0fc5eedc9af2d2bc1131
Davide Cristini <davcri91@gmail.com>
2019-08-11 10:45:23 +0100

    Respect config file to show/hide spaceship

--- Scenes/UILayer.gd
+++ Scenes/UILayer.gd
@@ -1,6 +1,11 @@
 extends CanvasLayer
 
+onready var spaceship = get_parent().get_node("Spaceship")
 
-
+func _ready():
+	if !$OptionsSubmenu.initialValues.showSpaceship:
+		spaceship.hide()
+		
 func _on_MainButtons_optionsButtonClicked():
 	$OptionsSubmenu.show()
+
