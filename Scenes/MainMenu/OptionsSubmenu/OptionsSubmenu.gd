6fb92e0c2d9e4fdc49a85f2f86af6b68ed7da1f2
Davide Cristini <davcri91@gmail.com>
2019-09-11 17:07:09 +0100

    Refactor OptionsSubmenu

--- Scenes/MainMenu/OptionsSubmenu/OptionsSubmenu.gd
+++ Scenes/MainMenu/OptionsSubmenu/OptionsSubmenu.gd
@@ -1,32 +1,34 @@
 extends Panel
 
-var initialValues = {}
 var SAVE_FILE_PATH = "res://settings.cfg" 
 
+var config: ConfigFile
+
 func _ready():
-	getSaveFile()
+	config = getConfigFile()
 	
-# this overrides the default show() method
+	# check if keys exist. If not, set them to default values
+	if not config.has_section_key("general", "playerName"):
+		config.set_value("general", "playerName", "")
+	if not config.has_section_key("general", "showSpaceship"):
+		config.set_value("general", "showSpaceship", true)
+	if not config.has_section_key("audio", "volume"):
+		config.set_value("audio", "volume", 0)
+		
+#	initialValues = getDictionaryFromConfigFile(config)
+	# Save the changes by overwriting the previous file
+	config.save(SAVE_FILE_PATH)	
+	
+
 func show():
-	# save variables state
-	initialValues = {
-		"showSpaceship": $VBoxContainer/ShowSpaceShip/CheckBox.pressed,
-		"volume": $VBoxContainer/Volume/HSlider.value,
-		"playerName": $VBoxContainer/PlayerName/LineEdit.text
-	}
+	"""This overrides the default show() method."""
 	visible = true
-	getSaveFile()
-	# reset initial values
-	$VBoxContainer/ShowSpaceShip/CheckBox.pressed = initialValues.showSpaceship
-	$VBoxContainer/Volume/HSlider.value = initialValues.volume
-	$VBoxContainer/PlayerName/LineEdit.text = initialValues.playerName
+	setSceneValuesFromConfig(config)
 
 func _on_Cancel_pressed():
 	hide()
 	# reset initial values
-	$VBoxContainer/ShowSpaceShip/CheckBox.pressed = initialValues.showSpaceship
-	$VBoxContainer/Volume/HSlider.value = initialValues.volume
-	$VBoxContainer/PlayerName/LineEdit.text = initialValues.playerName
+	setSceneValuesFromConfig(config)
 
 
 func _on_Ok_pressed():
@@ -36,43 +38,36 @@ func _on_Ok_pressed():
 		"volume": $VBoxContainer/Volume/HSlider.value,
 		"playerName": $VBoxContainer/PlayerName/LineEdit.text
 	}
-	saveConfig(data)
+	saveConfig(config, data)
 
-func getSaveFile():
+
+func getConfigFile() -> ConfigFile:
+	""" Loads the ConfigFile and returns it. 
+	If unsuccesfull returns an error code. 
+	"""
 	var config = ConfigFile.new()
-	# creates file if it does not exists
 	var f = File.new()
+	# creates file if it does not exists
 	if !f.file_exists(SAVE_FILE_PATH):
 		config.save(SAVE_FILE_PATH)
-	
+	# load the file
 	var err = config.load(SAVE_FILE_PATH)
-	if err == OK:	
-		# check if keys exist. If not, set them to default values
-		if not config.has_section_key("general", "playerName"):
-			config.set_value("general", "playerName", "")
-		if not config.has_section_key("general", "showSpaceship"):
-			config.set_value("general", "showSpaceship", true)
-		if not config.has_section_key("audio", "volume"):
-			config.set_value("audio", "volume", 0)
-		
-		initialValues = {
-			"playerName": config.get_value("general", "playerName"),
-			"showSpaceship": config.get_value("general", "showSpaceship"),
-			"volume": config.get_value("audio", "volume"),
-		}
-		# Save the changes by overwriting the previous file
-		config.save(SAVE_FILE_PATH)
+	# if file loaded
+	if err == OK:
+		# return it
+		return config
 	else:
 		print("Error while loading config", err)
+		return err
 
-func saveConfig(data):
-	var config = ConfigFile.new()
-	
-	# creates file if it does not exists
-	var f = File.new()
-	if !f.file_exists(SAVE_FILE_PATH):
-		config.save(SAVE_FILE_PATH)
-	
+
+func setSceneValuesFromConfig(config: ConfigFile):
+	$VBoxContainer/ShowSpaceShip/CheckBox.pressed = config.get_value("general", "showSpaceship")
+	$VBoxContainer/Volume/HSlider.value = config.get_value("audio", "volume")
+	$VBoxContainer/PlayerName/LineEdit.text = config.get_value("general", "playerName")
+
+
+func saveConfig(config, data):
 	var err = config.load(SAVE_FILE_PATH)
 	if err == OK:
 		config.set_value("general", "playerName", data.playerName)
--- Scenes/UILayer.gd
+++ Scenes/UILayer.gd
@@ -3,8 +3,12 @@ extends CanvasLayer
 onready var spaceship = get_parent().get_node("Spaceship")
 
 func _ready():
-	if !$OptionsSubmenu.initialValues.showSpaceship:
+	if !$OptionsSubmenu.config.get_value("general", "showSpaceship"):
 		spaceship.hide()
+	
+	$ProfileName.text = "Profile = " + $OptionsSubmenu.config.get_value("general", "playerName")
+	# TODO
+	$Money.text = "Money = " + str($OptionsSubmenu.config.get_value("general", "money"))
 		
 func _on_MainButtons_optionsButtonClicked():
 	$OptionsSubmenu.show()
